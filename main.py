44243acc1e084bf2ef3562e3a0f548d595db9fc8
SebastianBobes <bobes_sebastian03@yahoo.com>
2024-06-23 20:33:01 +0300

    added login feature to our program and also 2 new users

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+#create a quiz game with admin and players. a user has to login, if player then he can play
+#if admin can add quiestions
+import json
+import users
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name} dasdasdadadada')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    welcome_msg = "Welcome to Quiz Game"
+    print(f"{len(welcome_msg) * '='} {welcome_msg} {len(welcome_msg) * '='}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    users.login()
