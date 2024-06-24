ebaf06426093ddcd76c68ca6f7fba44c7256028f
SebastianBobes <bobes_sebastian03@yahoo.com>
2024-06-24 11:52:33 +0300

    commit added functionality for admin purposes

--- game.py
+++ game.py
@@ -20,14 +20,21 @@ def change_highscore(player_id: str, score: int, path: str = "users.json"):
     else:
         print("Successfully save the new high score!")
 
-
+def read_questions(questions_path: str = "questions.json") ->list:
+    try:
+        with open(questions_path, 'r') as f:
+            questions = json.loads(f.read())
+            questions = questions['questions']
+        return questions
+    except Exception as e:
+        print(f"Fatal error on reading quiz questions: {e}")
+        exit(1)
 
     pass
 def run_game(player:dict, questions_path :str = "questions.json")-> int:
     score = 0
-    with open(questions_path, 'r') as f:
-        questions = json.loads(f.read())
-        questions = questions['questions']
+    questions = read_questions(questions_path)
+    copy_questions = copy.deepcopy(questions)
 
     copy_questions = copy.deepcopy(questions)
     while copy_questions:
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #if admin can add quiestions
 import json
 import time
-
+import admin_functions
 import users
 import game
 
@@ -13,9 +13,12 @@ if __name__ == '__main__':
     current_player = users.login()
 
     while True:
-        print(f"Let's play {list(current_player.keys())[0]}")
-        game.run_game(current_player)
+        if list(list(current_player.keys())[0]) =='admin':
+            admin_functions.run()
+        else:
+            print(f"Let's play {list(current_player.keys())[0]}")
+            game.run_game(current_player)
+            user_pick = input("Do you want to play again? Y/N: ")
+            if user_pick.lower() == "n":
+                break
         time.sleep(2)
-        user_pick = input("Do you want to play again? Y/N: ")
-        if user_pick.lower() == "n":
-            break
